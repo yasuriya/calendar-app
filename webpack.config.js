b672725cfda63fdcfc25999063c5a87411c53a48
Anastasiia Shevtsova <anastasiia08@ukr.net>
2022-08-18 17:08:16 +0200

    configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const webpack = require('webpack')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   devtool: 'eval-source-map',
@@ -30,6 +31,9 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: '_redirects', to: '' }],
     }),
   ],
   devServer: {
